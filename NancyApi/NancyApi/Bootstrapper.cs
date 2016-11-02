bccc5d004d8459db8de08a89c4549687bff1d4db
Leonardo Lana <leolucas.lana@hotmail.com>
2016-11-01 22:32:22 -0200

    Subindo a aplicação construída NancyFx

---
+++ NancyApi/NancyApi/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Elmah;
+
+namespace NancyApi
+{
+	public class Bootstrapper : DefaultNancyBootstrapper
+	{
+		protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+		{
+			base.ApplicationStartup(container, pipelines);
+			Elmahlogging.Enable(pipelines, "elmah", new string[0], new[] { HttpStatusCode.NotFound, HttpStatusCode.InsufficientStorage });
+		}
+	}
+}
